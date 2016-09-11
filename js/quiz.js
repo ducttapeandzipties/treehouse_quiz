b2ac236d2859640e61761e74e5e162bda357d5f2
Dan Wagner <dan.nmi.wagner@gmail.com>
2016-09-10 22:13:45 -0400

    added scoring loop

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,10 @@
 var counter = 0;
+var score = 0;
+
 function print(message) {
   document.write(message);
 }
-var quizData = [
+var quizData = [           // array for qestions, answers and guesses.
   ["How many hours in a day?", 24],
   ["How many legs on a dog?", 4],
   ["How many days in a year?", 365],
@@ -11,5 +13,9 @@ var quizData = [
 
 while (counter < quizData.length ){
   quizData[counter].push(prompt(quizData[counter][0]));
+    if (quizData[counter][1] == quizData[counter][2]){
+      score += 1;
+    }
+
   counter += 1;
 }
