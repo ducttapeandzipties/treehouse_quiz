0d86074232dff27a0e4b1d782dfe68c8f729cfb2
Dan Wagner <dan.nmi.wagner@gmail.com>
2016-09-10 23:32:42 -0400

    fin!

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,12 @@
 var counter = 0;
 var score = 0;
+var htmlTrue = '<h3>You got these correct:</h3><ol type="I">';
+var htmlFalse = '<h3>You got these wrong:<h3><ol type="I">';
 
 function print(message) {
   document.write(message);
 }
-var quizData = [           // array for qestions, answers and guesses.
+var quizData = [                                          // array for qestions, answers and guesses.
   ["How many hours in a day?", 24],
   ["How many legs on a dog?", 4],
   ["How many days in a year?", 365],
@@ -12,10 +14,22 @@ var quizData = [           // array for qestions, answers and guesses.
 ]
 
 while (counter < quizData.length ){
-  quizData[counter].push(prompt(quizData[counter][0]));
-    if (quizData[counter][1] == quizData[counter][2]){
-      score += 1;
+  quizData[counter].push(prompt(quizData[counter][0]));   // prompt user for response, store in array
+    if (quizData[counter][1] == quizData[counter][2]){    //check response against data in array
+      htmlTrue += "<li>" + quizData[counter][0] + " " + quizData[counter][1] + "</li>";     // construct ordered list (true)
+      score += 1;                                         // add to numberical score
+    }
+    else {
+      htmlFalse += "<li>" + quizData[counter][0] + " " + quizData[counter][1] + "</li>";    //construct orded list (false)
+
     }
 
-  counter += 1;
+  counter += 1;                                           //increment score
 }
+
+htmlTrue += "</ol>";                                      //close ordered lists
+htmlFalse += "</ol>";
+
+print("You got " + score + " correct.");                  //output
+print(htmlTrue);
+print(htmlFalse);
