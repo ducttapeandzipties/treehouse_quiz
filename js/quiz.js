c2294fed4045d076c8e943d4fcd66a21d8e73b5d
Dan Wagner <dan.nmi.wagner@gmail.com>
2016-09-10 22:00:06 -0400

    added prompt loop

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,4 @@
-
+var counter = 0;
 function print(message) {
   document.write(message);
 }
@@ -7,4 +7,9 @@ var quizData = [
   ["How many legs on a dog?", 4],
   ["How many days in a year?", 365],
   ["How many wheels on a car?", 4]
-] 
+]
+
+while (counter < quizData.length ){
+  quizData[counter].push(prompt(quizData[counter][0]));
+  counter += 1;
+}
