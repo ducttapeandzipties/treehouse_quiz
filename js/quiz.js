83b1ffafa35d9d997c588013280732358858184e
Dan Wagner <dan.nmi.wagner@gmail.com>
2016-09-10 21:31:18 -0400

    added 2d array

---
+++ js/quiz.js
@@ -0,0 +1,10 @@
+
+function print(message) {
+  document.write(message);
+}
+var quizData = [
+  ["How many hours in a day?", 24],
+  ["How many legs on a dog?", 4],
+  ["How many days in a year?", 365],
+  ["How many wheels on a car?", 4]
+] 
